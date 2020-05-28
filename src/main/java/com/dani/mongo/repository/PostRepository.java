451b02302aadf69275b0feab845c30d1d2ae2f1a
Daniella Santiago <danii.santiago@gmail.com>
2020-05-28 17:42:10 -0300

    consulta simples com @Query

--- src/main/java/com/dani/mongo/repository/PostRepository.java
+++ src/main/java/com/dani/mongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.dani.mongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.dani.mongo.domain.Post;
@@ -10,5 +11,8 @@ import com.dani.mongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	@Query("{'title': { $regex: ?0, $options: 'i'}}") //?0 primeiro parametro
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
