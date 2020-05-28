5f0b2b6aea5fca30fb4d6a71f33845fc03b7ef44
Daniella Santiago <danii.santiago@gmail.com>
2020-05-28 16:34:17 -0300

    projeção dos dados do autor com DTO

--- src/main/java/com/dani/mongo/config/Instantiation.java
+++ src/main/java/com/dani/mongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.dani.mongo.domain.Post;
 import com.dani.mongo.domain.User;
+import com.dani.mongo.dto.AuthorDTO;
 import com.dani.mongo.repository.PostRepository;
 import com.dani.mongo.repository.UserRepository;
 
@@ -35,12 +36,12 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		userRepository.saveAll(Arrays.asList(maria, alex, bob)); 
-		postRepository.saveAll(Arrays.asList(post1, post2));
-		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+				 
+		postRepository.saveAll(Arrays.asList(post1, post2));		
 	}
 
 }
