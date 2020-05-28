65eb6812863a1d44c36de16973ef9f179bd6bbbb
Daniella Santiago <danii.santiago@gmail.com>
2020-05-27 23:40:14 -0300

    obtendo um usuario por id

--- src/main/java/com/dani/mongo/services/UserService.java
+++ src/main/java/com/dani/mongo/services/UserService.java
@@ -1,20 +1,27 @@
 package com.dani.mongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dani.mongo.domain.User;
 import com.dani.mongo.repository.UserRepository;
+import com.dani.mongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
-	@Autowired //vai instaciar o objeto automatico
+	@Autowired // vai instaciar o objeto automatico
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
