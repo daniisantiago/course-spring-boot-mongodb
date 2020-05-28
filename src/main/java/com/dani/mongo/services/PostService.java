22187a3d7eef3186a83c3897f8a0a8a436fca935
Daniella Santiago <danii.santiago@gmail.com>
2020-05-28 18:06:11 -0300

    consulta com varios criterios

--- src/main/java/com/dani/mongo/services/PostService.java
+++ src/main/java/com/dani/mongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.dani.mongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,7 +23,12 @@ public class PostService {
 	}
 	
 	public List<Post> findyByTitle(String text){
-		return repo.searchTitle(text);
-		//return repo.findByTitleContainingIgnoreCase(text); 
+		return repo.searchTitle(text); //consulta com @query
+		//return repo.findByTitleContainingIgnoreCase(text);  //consulta com query methods
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000); //acrescentando um dia a data maxima
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 }
